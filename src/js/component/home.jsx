e278e4d52fe94a9d2abb492eca61a74b47a01692
kacijcox <145575906+kacijcox@users.noreply.github.com>
2023-12-10 17:44:14 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,84 +1,129 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [inputValue, setInputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+  
+   /*/
   useEffect(() => {
-    // Fetch API code to get initial data
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/alesanchezr")
-      .then((response) => response.json())
-      .then((data) => setTodos(data))
-      .catch((error) => console.error("Error fetching initial data:", error));
-  }, []); // Empty dependency array ensures this effect runs only once, similar to componentDidMount
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://playground.4geeks.com/apis/fake/todos/user/kcox");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+  
+        const data = await response.json();
+        setTodos(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  
 
-  const syncWithServer = () => {
-    // fetch API code to sync data with the server
-    fetch("https://playground.4geeks.com/apis/fake/todos/user/alesanchezr", {
-      method: "PUT",
-      body: JSON.stringify(todos),
+  const fetchTasks = () => {
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/")
+      .then((resp) => resp.json())
+      .then((data) => {
+        // Update the local state with the fetched tasks
+        setTodos(data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.log(error);
+      });
+  };
+
+  const createTask = () => {
+    // Add a new task to the server
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/", {
+      method: "POST",
+      body: JSON.stringify([]), // Pass an empty array since there are no todos yet
       headers: {
         "Content-Type": "application/json",
       },
     })
       .then((resp) => resp.json())
-      .then((data) => console.log("Data synced with server:", data))
-      .catch((error) => console.error("Error syncing with server:", error));
+      .then((data) => {
+        // Update the local state with the new tasks from the server
+        setTodos([]);
+        setInputValue(""); // Clear input after adding task
+      })
+      .catch((error) => {
+        // Handle errors
+        console.log(error);
+      });
   };
 
-  const handleAddTodo = () => {
-    // Add new todo locally
-    setTodos((prevTodos) => [...prevTodos, inputValue]);
-    setInputValue("");
-
-    // Sync with the server
-    syncWithServer();
+ 
+  const deleteTask = (index) => {
+    // Delete a task from the server
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/kcox", {
+      method: "PUT",
+      body: JSON.stringify(todos.filter((_, currentIndex) => index !== currentIndex)),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        // Update the local state with the new tasks from the server
+        setTodos(data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.log(error);
+      });
   };
 
-  const handleDeleteTodo = (index) => {
-    // Delete todo locally
-    setTodos((prevTodos) => prevTodos.filter((_, currentIndex) => index !== currentIndex));
-
-    // Sync with the server
-    syncWithServer();
+  const clearAllTasks = () => {
+    // Clear all tasks on the server
+    fetch("https://playground.4geeks.com/apis/fake/todos/user/kcox", {
+      method: "DELETE", // Use DELETE method for clearing all tasks
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        // Update the local state with an empty list
+        setTodos([]);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.log(error);
+      });
   };
-
-  const handleCleanAllTasks = () => {
-    // Clean all tasks locally
-    setTodos([]);
-
-    // Sync with the server to delete all tasks
-    syncWithServer();
-  };
-
-  return (
-    <div className="container">
-      <h1>My Todos</h1>
-      <ul>
-        <li>
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyUp={(e) => {
-              if (e.key === "Enter") {
-                handleAddTodo();
-              }
-            }}
-            placeholder="What do you need to do?!?"
-          />
-        </li>
+ /*/
+ return (
+  <div className="container">
+    <h1>My Todos</h1>
+    <ul>
+      <li>
+        <input 
+        type="text" 
+        onChange={(e) => setInputValue(e.target.value)}
+        value={inputValue}
+        onKeyUp={(e) => {
+          if (e.key === "Enter") {
+          setTodos((prevTodos) => [...prevTodos, inputValue]);
+          setInputValue("");
+          }
+        }}
+        placeholder="What do you need to do?!?"></input></li>
         {todos.map((item, index) => (
-          <li key={index}>
-            {item}
-            <i className="fa-solid fa-trash" onClick={() => handleDeleteTodo(index)}></i>
-          </li>
+        <li>{item}<i class="fa-solid fa-trash" onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}></i></li>
         ))}
-      </ul>
-      <button onClick={handleCleanAllTasks}>Clean All Tasks</button>
-      <div>{todos.length}</div>
-    </div>
-  );
+    </ul>
+    <div>{todos.length}</div>
+  </div>
+);
 };
 
 export default Home;
+
+
